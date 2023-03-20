5a2545bc87675e06b988c820ede93a414ebde107
Petr Krysl <pkrysl@ucsd.edu>
2023-03-20 14:32:13 -0700

    add a function to load data

--- test/perf2.jl
+++ test/perf2.jl
@@ -6,25 +6,27 @@ using ProfileView
 using DataDrop
 using Test
 
-function testA()
-    I = DataDrop.retrieve_matrix(joinpath(pwd(), "test", "I.h5"))
-    J = DataDrop.retrieve_matrix(joinpath(pwd(), "test", "J.h5"))
-    V = DataDrop.retrieve_matrix(joinpath(pwd(), "test", "V.h5"))
+function load_data()
+    I = DataDrop.retrieve_matrix(joinpath(pwd(), "test", "h8", "I.h5"))
+    J = DataDrop.retrieve_matrix(joinpath(pwd(), "test", "h8", "J.h5"))
+    V = DataDrop.retrieve_matrix(joinpath(pwd(), "test", "h8", "V.h5"))
     N = 1328319
+    return N, I, J, V
+end
+
+function testA()
+    N, I, J, V = load_data()
 
     @time let
         A = sparse(I, J, V, N, N)
     end
 end
 function testB()
-    I = DataDrop.retrieve_matrix(joinpath(pwd(), "test", "I.h5"))
-    J = DataDrop.retrieve_matrix(joinpath(pwd(), "test", "J.h5"))
-    V = DataDrop.retrieve_matrix(joinpath(pwd(), "test", "V.h5"))
-    N = 1328319
+    N, I, J, V = load_data()
 
     @time let
         B = SimplySparse.sparse(I, J, V, N, N)
-        # @profview SimplySparse.sparse(I, J, V, N, N)
+        # @profview SimplySparse.sparse(I, J, V, N, N)  #
         # @show nnz(B)
     end
 end
