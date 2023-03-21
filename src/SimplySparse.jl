2372810198abeb4b0321aa6cb0c53c457229d7ab
Petr Krysl <pkrysl@ucsd.edu>
2023-03-21 13:10:04 -0700

    test for versions

--- src/SimplySparse.jl
+++ src/SimplySparse.jl
@@ -4,7 +4,11 @@ using Random
 using SparseArrays
 using Base: require_one_based_indexing
 
-include("impl4.jl")
+@static if VERSION > v"1.8.5"
+    include("impl4.jl")
+else
+    include("impl2.jl")
+end
 
 sparse(I,J,V::AbstractVector,m,n) = sparse(I, J, V, Int(m), Int(n), +)
 sparse(I,J,V::AbstractVector{Bool},m,n) = sparse(I, J, V, Int(m), Int(n), |)
