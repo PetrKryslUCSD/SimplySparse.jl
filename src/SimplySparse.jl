99152068522a90974d0b5df3e3029accdad013f3
Petr Krysl <pkrysl@ucsd.edu>
2023-03-25 19:52:19 -0700

    update parallel implementation

--- src/SimplySparse.jl
+++ src/SimplySparse.jl
@@ -4,11 +4,13 @@ using Random
 using SparseArrays
 using Base: require_one_based_indexing
 
+
+
 @static if VERSION > v"1.8.5"
     include("impl3.jl")
 else
     include("impl2.jl")
-    include("par_impl2.jl")
+    include("par_impl3.jl")
 end
 
 end # module SparseSparse
